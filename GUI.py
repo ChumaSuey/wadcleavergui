21df56e99b531edb6f046cbcfb18402c0914263c
ChumaSuey <hyperghostblade@gmail.com>
2023-11-17 08:05:59 -0400

    Token and Delimiter added by Nepta

--- GUI.py
+++ GUI.py
@@ -4,40 +4,66 @@ import subprocess
 import os
 from WADCleaver import *
 
-#Function Definitions:
+# Function Definitions:
+
 
 def select_wad_file():
     wad_file = filedialog.askopenfilename(filetypes=[("WAD Files", "*.wad")])
     wad_entry.delete(0, tk.END)
     wad_entry.insert(0, wad_file)
 
+
 def select_folder():
     folder = filedialog.askdirectory()
     folder_entry.delete(0, tk.END)
     folder_entry.insert(0, folder)
 
+# Added by Nepta, callback function
+def callback(P):
+    if P == "":
+        return True
+    if len(P) == 1 and P[0] == "-":
+        return True
+    try:
+        int(P)
+        return True
+    except Exception:
+        return False
+
+
 def generate_wad_folder():
     wad_file = wad_entry.get()
     folder_name = folder_entry.get()
+    delim = delim_entry.get()
+    token = token_entry.get()
 
     if wad_file and folder_name:
         if not os.path.exists(folder_name):
-         os.makedirs(folder_name)
-         subprocess.call(["python", "Wadcleaver.py", wad_file, folder_name]) # Añadi un "-o" como parametro pero no sirvio
-            # Aquí puedes agregar la lógica para separar los wads en colores
+            os.makedirs(folder_name)
+        params = ["python", "WADCleaver.py", wad_file, folder_name]
+        if delim:
+            params.extend(["--delim", delim])
+        if token:
+            params.extend(["--token", token])
+        subprocess.call(params)
 
-        result_label.config(text="Carpeta generada con éxito")
+        # Aquí puedes agregar la lógica para separar los wads en colores
+
+        result_label.config(text="WARDS generada con éxito")
     else:
-        result_label.config(text="Por favor, selecciona un archivo .wad y proporciona un nombre de carpeta")
+        result_label.config(
+            text="Por favor, selecciona un archivo .wad y proporciona un nombre de carpeta")
 
 
 window = tk.Tk()
-intro = tk.Label(text="This is intended to be the GUI for the use of WADCleaver")
+intro = tk.Label(
+    text="This is intended to be the GUI for the use of WADCleaver")
 window.title("WADCleaver UI")
 intro.pack()
-window.geometry("400x200")
+window.geometry("600x300") #from 500x300 to 600x300 for a bit better resolution
 
-wad_label = tk.Label(window, text="Select the wad file") #Selecciona el archivo .wad:
+# Select the wad file:
+wad_label = tk.Label(window, text="Select the wad file")
 wad_label.pack()
 
 wad_entry = tk.Entry(window, width=50)
@@ -46,26 +72,43 @@ wad_entry.pack()
 wad_button = tk.Button(window, text="Select file", command=select_wad_file)
 wad_button.pack()
 
-folder_label = tk.Label(window, text="Select the folder to save the separated WADs")
+folder_label = tk.Label(
+    window, text="Select the folder to save the separated WADs")
 folder_label.pack()
 
 folder_entry = tk.Entry(window, width=50)
 folder_entry.pack()
 
-
-folder_button = tk.Button(window, text="Select directory", command=select_folder)
+folder_button = tk.Button(
+    window, text="Select directory", command=select_folder)
 folder_button.pack()
+#Delimiter added by Nepta
+delim_label = tk.Label(window, text="Delimiter")
+delim_label.pack()
 
-message_label = tk.Label(window, text="Reminder: the split WADs will be generated in the location above")
+delim_entry = tk.Entry(window, width=50)
+delim_entry.pack()
+
+#Token added by Nepta
+token_label = tk.Label(window, text="Token")
+token_label.pack()
+vcmd = (window.register(callback))
+token_entry = tk.Entry(
+    window, width=50, validate='all', validatecommand=(vcmd, '%P')
+)
+token_entry.pack()
+
+
+
+message_label = tk.Label(
+    window, text="Reminder: the split WADs will be generated in the location above")
 message_label.pack()
 
-generate_button = tk.Button(window, text="Generate folder", command=generate_wad_folder)
+generate_button = tk.Button(
+    window, text="Generate WAD", command=generate_wad_folder)
 generate_button.pack()
 
 result_label = tk.Label(window, text="")
 result_label.pack()
 
 window.mainloop()
-
-
-
